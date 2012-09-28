73966259db7bb9cc7b7f6c8f71af82df46bda3cf
Andreas Koestler <andreas.koestler@gmail.com>
2012-09-28 19:21:48 +1000

    Added some error checking.

--- unistd.lisp
+++ unistd.lisp
@@ -8,95 +8,87 @@
  
 (cffi:use-foreign-library libc)
 
-@export
-(defconstant B0 #o0) 
-@export
-(defconstant B50 #o1)
-@export
-(defconstant B75 #o2)
-@export
-(defconstant B110 #o3)
-@export
-(defconstant B134 #o4)
-@export
-(defconstant B150 #o5)
-@export
-(defconstant B200 #o6)
-@export
-(defconstant B300 #o7)
-@export
-(defconstant B600 #o10)
-@export
-(defconstant B1200 #o11)
-@export
-(defconstant B1800 #o12)
-@export
-(defconstant B2400 #o13)
-@export
-(defconstant B4800 #o14)
-@export
-(defconstant B9600 #o15)
-@export
-(defconstant B19200 #o16)
-@export
-(defconstant B38400 #o17)
-@export
-(defconstant B57600 #o10001)
-@export
-(defconstant B115200 #o10002)
-@export
-(defconstant B230400 #o10003)
-@export
-(defconstant B460800 #o10004)
-@export
-(defconstant B500000 #o10005)
-@export
-(defconstant B576000 #o10006)
-@export
-(defconstant B921600 #o10007)
-@export
-(defconstant B1000000 #o10010)
-@export
-(defconstant B1152000 #o10011)
-@export
-(defconstant B1500000 #o10012)
-@export
-(defconstant B2000000 #o10013)
-@export
-(defconstant B2500000 #o10014)
-@export
-(defconstant B3000000 #o10015)
-@export
-(defconstant B3500000 #o10016)
-@export
-(defconstant B4000000 #o10017)
 
-@export
-(defvar ONOCTTY #o00400)	     
+;;;
+;;; Constants, flgas, etc...
+;;;
 
-@export
-(defvar ONONBLOCK #o04000)
-
-@export
-(defvar ORDWR #o00002)
-
-@export
-(defvar FSETFL #o00004)
-
-
-@export
-(defun close (fd)
-  "The close() function shall deallocate the file descriptor indicated by fildes."
-  (foreign-funcall "close" :int fd :int))
-
-@export
-(defun open (filename flags &optional mode)
-  "The open function creates and returns a new file descriptor for the file named by filename. Initially, the file position indicator for the file is at the beginning of the file. The argument mode is used only when a file is created, but it doesn't hurt to supply the argument in any case."
-  (with-foreign-string (fn filename)
-    (foreign-funcall "open" :string fn :int flags :int (or mode 0) :int)))
-
+@export (defconstant CLOCAL    #o0004000)
+@export (defconstant CREAD     #o0000200)
+@export (defconstant ECHO      #o0000010)
+@export (defconstant ECHOE     #o0000020)
+@export (defconstant ECHOK     #o0000040)
+@export (defconstant ECHONL    #o0000100)
+@export (defconstant ISIG      #o0000001)
+@export (defconstant ICANON    #o0000002)
+@export (defconstant INLCR     #o0000100)
+@export (defconstant IGNCR     #o0000200)
+@export (defconstant ICRNL     #o0000400)
+@export (defconstant IEXTEN    #o0100000)
+@export (defconstant IGNBRK    #o0000001)
+@export (defconstant OPOST     #o0000001)
+@export (defconstant PARMRK    #o0000010)
+@export (defconstant B0        #o0000000) 
+@export (defconstant B50       #o0000001)
+@export (defconstant B75       #o0000002)
+@export (defconstant B110      #o0000003)
+@export (defconstant B134      #o0000004)
+@export (defconstant B150      #o0000005)
+@export (defconstant B200      #o0000006)
+@export (defconstant B300      #o0000007)
+@export (defconstant B600      #o0000010)
+@export (defconstant B1200     #o0000011)
+@export (defconstant B1800     #o0000012)
+@export (defconstant B2400     #o0000013)
+@export (defconstant B4800     #o0000014)
+@export (defconstant B9600     #o0000015)
+@export (defconstant B19200    #o0000016)
+@export (defconstant B38400    #o0000017)
+@export (defconstant B57600    #o0010001)
+@export (defconstant B115200   #o0010002)
+@export (defconstant B230400   #o0010003)
+@export (defconstant B460800   #o0010004)
+@export (defconstant B500000   #o0010005)
+@export (defconstant B576000   #o0010006)
+@export (defconstant B921600   #o0010007)
+@export (defconstant B1000000  #o0010010)
+@export (defconstant B1152000  #o0010011)
+@export (defconstant B1500000  #o0010012)
+@export (defconstant B2000000  #o0010013)
+@export (defconstant B2500000  #o0010014)
+@export (defconstant B3000000  #o0010015)
+@export (defconstant B3500000  #o0010016)
+@export (defconstant B4000000  #o0010017)
+@export (defconstant ONOCTTY   #o0000400)	     
+@export (defconstant ONONBLOCK #o0004000)
+@export (defconstant ORDWR     #o0000002)
+@export (defconstant FSETFL    #o0000004)
 
 (defconstant +NCCS+ 32)
+
+;;;
+;;; Some other cruft
+;;;
+
+(defcvar "errno" :int)
+
+(defun strerror (errno)
+  (foreign-funcall "strerror" :int errno :string))
+
+(define-condition c-error (error)
+  ((text :initarg :text :reader text))
+  (:report (lambda (condition stream)
+	     (format stream "~A: ~A" (text condition) (strerror *errno*)))))
+
+(defun partial (f &rest args)
+  "Takes a function f and fewer than the normal arguments to f, and
+  returns a fn that takes a variable number of additional args. When
+  called, the returned function calls f with args + additional args."
+  (case (length args)
+    (1 (lambda (&rest more) (apply f (cons (first args) more))))
+    (2 (lambda (&rest more) (apply f (cons (first args) (cons (second args) more)))))
+    (3 (lambda (&rest more) (apply f (cons (first args) (cons (second args) (cons (third args) more))))))
+    (t (lambda (&rest more) (apply f (concatenate 'list args more))))))
 
 (cffi:defctype tcflag_t :uint)
 
@@ -112,7 +104,6 @@
 
 (make-foreign-termios-struct)
 
-
 (defclass <termios> ()
   ((iflag :initform 0 :initarg :iflag :accessor iflag)
    (oflag :initform 0 :initarg :oflag :accessor oflag)
@@ -126,11 +117,36 @@
 	    collect (mem-aref cc 'cc_t i))))
     (make-instance '<termios> :iflag iflag :oflag oflag :cflag cflag :lflag lflag :cc cc)))
 
+(defmacro with-errno-checking (&optional opts ffcall)
+  (destructuring-bind (&optional (test '>) (comp -1)) 
+      opts
+    `(let ((res ,ffcall))
+       (unless (,test res ,comp)
+	 (error 'c-error :text (format nil "Error in ffcall")))
+       res)))
+
+;;;
+;;; Exported posix funs
+;;;
+
+@export
+(defun close (fd)
+  "The close() function shall deallocate the file descriptor indicated by fildes."
+  (with-errno-checking ()
+      (foreign-funcall "close" :int fd :int)))
+
+@export
+(defun open (filename flags &optional mode)
+  "The open function creates and returns a new file descriptor for the file named by filename. Initially, the file position indicator for the file is at the beginning of the file. The argument mode is used only when a file is created, but it doesn't hurt to supply the argument in any case."
+  (with-foreign-string (fn filename)
+    (with-errno-checking ()
+	(foreign-funcall "open" :string fn :int flags :int (or mode 0) :int))))
+
 @export
 (defun tcgetattr (fd)
   (with-foreign-object (ptr 'ftermios)
-    ;; TODO Error handling!!
-    (print (foreign-funcall "tcgetattr" :int fd ftermios ptr :int))
+    (with-errno-checking ()
+	(foreign-funcall "tcgetattr" :int fd ftermios ptr :int))
     (with-foreign-slots  ((c_iflag c_oflag c_cflag c_lflag c_cc) ptr ftermios)
       (make-termios c_iflag c_oflag c_cflag c_lflag c_cc))))
 
@@ -145,66 +161,14 @@
       (loop for i below +NCCS+
 	 for el in (cc termios)
 	 do (setf (mem-aref c_cc 'cc_t i) el)))
-    ;; TODO Error handling!!
-    (foreign-funcall "tcsetattr" :int fd :int (or actions 0) ftermios ptr :int)))
-
-
-
+    (with-errno-checking ()
+	(foreign-funcall "tcsetattr" :int fd :int (or actions 0) ftermios ptr :int))))
 
 @export
-(defconstant CLOCAL    #o0004000)
-
-@export
-(defconstant CREAD     #o0000200)
-
-@export
-(defconstant ECHO      #o0000010)
-
-@export
-(defconstant ECHOE     #o0000020)
-
-@export
-(defconstant ECHOK     #o0000040)
-
-@export
-(defconstant ECHONL    #o0000100)
-
-@export
-(defconstant ISIG      #o0000001)
-
-@export
-(defconstant ICANON    #o0000002)
-
-@export
-(defconstant INLCR     #o0000100)
-
-@export
-(defconstant IGNCR     #o0000200)
-
-@export
-(defconstant ICRNL     #o0000400)
-
-@export
-(defconstant IEXTEN    #o0100000)
-
-@export
-(defconstant IGNBRK    #o0000001)
-
-@export
-(defconstant OPOST     #o0000001)
-
-@export
-(defconstant PARMRK    #o0000010)
-
-
-#+xxx(defun cfsetispeed (speed &optional termios)
-  (sb-posix:cfsetispeed speed termios))
-
-#+xxx(defun cfsetospeed (speed &optional termios) 
-  (sb-posix:cfsetospeed speed termios))
-
-#+xxx(defun fcntl (fd cmd &optional arg)
-  (sb-posix:fcntl fd cmd arg))
+(defun fcntl (fd cmd &optional args)
+  (with-errno-checking ()
+      (apply (partial 'foreign-funcall
+		      "fcntl" :int fd :int cmd) args)))
 
 #+xxx(defun tcflush (fd queue-selector)
   (sb-posix:tcflush fd queue-selector))
