0ab5e6b24fe10fefbf396ec96b419e6828b96bed
Andreas Koestler <andreas.koestler@gmail.com>
2012-09-29 13:36:39 +1000

    Shadow read/write

--- package.lisp
+++ package.lisp
@@ -9,7 +9,7 @@
   (:use #:cl 
 	#:annot.class
 	#+(not sbcl) #:cffi)
-  (:shadow #:cl #:close #:open))
+  (:shadow #:cl #:close #:open #:write #:read))
    
 
 #+(and sbcl (or linux unix)) (do-external-symbols (s (find-package :unistd))
